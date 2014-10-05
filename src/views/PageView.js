7d8a7ce2350cff44a12f6cc4c088e4fb595aff4a
foliveira <fabio.an.oliveira@gmail.com>
2014-10-05 21:37:12 +0100

    A version with the basic layout

--- src/views/PageView.js
+++ src/views/PageView.js
@@ -2,10 +2,15 @@ define(function(require, exports, module) {
     var View = require('famous/core/View');
     var Surface = require('famous/core/Surface');
     var Transform = require('famous/core/Transform');
+    var RenderNode = require('famous/core/RenderNode');
     var StateModifier = require('famous/modifiers/StateModifier');
     var HeaderFooter = require('famous/views/HeaderFooterLayout');
     var ImageSurface = require('famous/surfaces/ImageSurface');
     var FastClick = require('famous/inputs/FastClick');
+    var Utility = require('famous/utilities/Utility');
+    var ScrollContainer = require('famous/views/ScrollContainer');
+
+    var moment = require('moment');
 
     function PageView() {
         View.apply(this, arguments);
@@ -90,15 +95,108 @@ define(function(require, exports, module) {
     }
 
     function _createBody() {
-        this.bodySurface = new Surface({
-            size : [undefined, undefined],
+        this.monthDetail = new Surface({
+          content: "Oct, 14",
+          size: [undefined, 20],
+          properties: {
+            backgroundColor: "#20202a",
+            color: '#fff'
+          }
+        })
+
+        this.dateScroll = new ScrollContainer({
+            size : [undefined, 75],
             properties: {
-              backgroundColor: "#00000a"
+              backgroundColor: "#20202a",
+              color: '#fff'
+            },
+            scrollview : {
+              paginated: true,
+              groupScroll: true,
+              direction: Utility.Direction.X
             }
         });
+        var surfaces = [];
+        this.dateScroll.sequenceFrom(surfaces);
 
+        for (var i = 0, temp; i < 3; i++) {
 
-        this.layout.content.add(this.bodySurface);
+            item = new Surface({
+                 content: (i + 1),
+                 size: [undefined, 50],
+                 properties: {
+                     backgroundColor: "hsl(" + (i * 360 / 3) + ", 100%, 50%)",
+                     textAlign: "center"
+                 }
+            });
+
+            surfaces.push(item);
+        }
+
+        this.gameDetails = new Surface({
+          content: 'Game detail',
+          size : [undefined, 78],
+          properties: {
+            backgroundColor: "#20202a",
+            color: '#fff'
+          },
+        });
+
+        this.hoursScroll = new ScrollContainer({
+            size : [undefined, 374],
+            properties: {
+              backgroundColor: "#fff"
+            },
+            scrollview: {
+              direction: Utility.Direction.Y
+            }
+        });
+        var hours = [];
+        this.hoursScroll.sequenceFrom(hours);
+        function __getHours(i) {
+          return moment().hour(Math.floor(i/2))
+                          .minute(i*30%60)
+                          .format("hh:mm A");
+          }
+
+        for (var i = 0, temp; i < 48; i++) {
+
+            var item = new Surface({
+                 content: __getHours(i),
+                 size: [undefined, 40],
+                 properties: {
+                     backgroundColor: "hsl(" + (i * 360 / 48) + ", 100%, 50%)",
+                     textAlign: "left"
+                 }
+            });
+
+            hours.push(item);
+        }
+
+        var monthDetailModifier = new StateModifier({
+          align: [0, 0.01],
+
+        })
+        var dateModifier = new StateModifier({
+            align: [0, 0.05],
+            //transform: Transform.translate(0, -5, 0)
+        });
+
+        var detailsModifier = new StateModifier({
+            align: [0, 0.15],
+            transform: Transform.translate(0, 0, 0)
+        });
+
+        var hoursModifier = new StateModifier({
+            align: [0, 0.05],
+            //origin: [0, 0.3],
+            transform: Transform.translate(0, 120, 0)
+        });
+
+        this.layout.content.add(monthDetailModifier).add(this.monthDetail);
+        this.layout.content.add(dateModifier).add(this.dateScroll);
+        this.layout.content.add(detailsModifier).add(this.gameDetails);
+        this.layout.content.add(hoursModifier).add(this.hoursScroll);
     }
 
     module.exports = PageView;
